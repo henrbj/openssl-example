875c92378658060e4ef547016060eae94f8bf2f0
henrbj <henrik_bjorkheim@hotmail.com>
2016-09-12 13:34:02 +0200

    Testing new tests

--- tests/crypto_test.cpp
+++ tests/crypto_test.cpp
@@ -11,4 +11,12 @@ int main() {
   // Results copied from http://www.freecodeformat.com/pbkdf2.php
   assert(Crypto::hex(Crypto::pbkdf2("Password", "Salt", 4096, 128 / 8)) == "f66df50f8aaa11e4d9721e1312ff2e66");
   assert(Crypto::hex(Crypto::pbkdf2("Password", "Salt", 8192, 512 / 8)) == "a941ccbc34d1ee8ebbd1d34824a419c3dc4eac9cbc7c36ae6c7ca8725e2b618a6ad22241e787af937b0960cf85aa8ea3a258f243e05d3cc9b08af5dd93be046c");
+  
+  
+  assert(Crypto::hex(Crypto::md5("test")) == "098f6bcd4621d373cade4e832627b4f6");
+  
+  assert(Crypto::hex(Crypto::sha256("test")) == "9f86d081884c7d659a2feaa0c55ad015a3bf4f1b2b0b822cd15d6c15b0f00a08");
+  
+  assert(Crypto::hex(Crypto::sha512("test")) == "ee26b0dd4af7e749aa1a8ee3c10ae9923f618980772e473f8819a5d4940e0db27ac185f8a0e1d5f84f88bc887fd67b143732c304cc5fa9ad8e6f57f50028a8ff");
+  
 }
